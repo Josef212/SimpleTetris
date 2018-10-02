788b32f64a497acec0e81f670c2345619beeee5c
Josef21296 <jcasanovasv@hotmail.com>
2018-10-02 22:28:13 +0200

    Picture and comment

--- SimpleTetris/SimpleTetris/main.cpp
+++ SimpleTetris/SimpleTetris/main.cpp
@@ -1,3 +1,7 @@
+/**
+	A simple tetris game made following the tutorial: http://javilop.com/gamedev/tetris-tutorial-in-c-platform-independent-focused-in-game-logic-for-beginners/
+*/
+
 #include <iostream>
 #include <SDL.h>
 
